5fd52ff0cc0253e1f85c031c2e2405daf9a3fdd8
BingLi <feather812002@163.com>
2016-12-23 02:00:00 +0800

    every push

--- PusmicMahJong/grails-app/services/com/pusmic/game/WebsokectService.groovy
+++ PusmicMahJong/grails-app/services/com/pusmic/game/WebsokectService.groovy
@@ -1,7 +1,9 @@
 package com.pusmic.game
 
 import grails.transaction.Transactional
-//import org.springframework.messaging.simp.SimpMessagingTemplate
+import org.springframework.messaging.simp.SimpMessagingTemplate
+
+
 @Transactional
 class WebsokectService {
 
